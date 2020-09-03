eda8ab6349e1cfa789e0fef4fcfd3221bb65ba47
Varun Agrawal <varagrawal@gmail.com>
2020-09-03 00:10:46 -0400

    main driver of Python code, and ignore generated files

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 """ Example python module. """
 
 import gtsam
+import gtsam_example
 
 # pylint: disable = no-member
 
@@ -13,3 +14,13 @@ def meaning_of_everything():
 def create_special_2d_pose():
     """ Create a pose with (x,y) = (1,2) and theta = 3."""
     return gtsam.Pose2(1, 2, 3)
+
+
+def greet():
+    greeter = gtsam_example.Greeting()
+    greeter.sayHello()
+    greeter.sayGoodbye()
+
+
+if __name__ == "__main__":
+    greet()
