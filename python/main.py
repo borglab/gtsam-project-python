7ab16740567eefddd05607f4a3ba1ea45e5dbc3a
Varun Agrawal <varagrawal@gmail.com>
2020-09-03 00:28:50 -0400

    add argparse and formatting

--- python/main.py
+++ python/main.py
@@ -1,6 +1,9 @@
 """ Example python module. """
 
+import argparse
+
 import gtsam
+
 import gtsam_example
 
 # pylint: disable = no-member
@@ -17,10 +20,28 @@ def create_special_2d_pose():
 
 
 def greet():
+    """
+    Call the wrapped Greeting code so it can say Hello and Goodbye.
+    """
     greeter = gtsam_example.Greeting()
     greeter.sayHello()
     greeter.sayGoodbye()
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser("Main runner for GTSAM-wrapped code")
+    parser.add_argument("command",
+                        help="The function to execute \
+                            (e.g. 'meaning_of_everything', 'create_special_2d_pose', 'greet')")
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    greet()
+    func_map = {
+        'meaning_of_everything': meaning_of_everything,
+        'create_special_2d_pose': create_special_2d_pose,
+        'greet': greet
+    }
+    args = parse_arguments()
+    func = func_map.get(args.command, meaning_of_everything)
+    print(func())
